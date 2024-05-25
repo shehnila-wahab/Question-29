72645271a3056c1ed271300bf9eb825454ffa589
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:19:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var fav_fruits = ["apple", "orange", "grapes"];
+if (fav_fruits.includes('apple')) {
+    console.log("I really like apple!");
+}
+if (fav_fruits.includes('orange')) {
+    console.log("I really like orange!");
+}
+if (fav_fruits.includes('mango')) {
+    console.log("I really like mango!");
+}
+if (fav_fruits.includes('grapes')) {
+    console.log("I really like grapes!");
+}
+if (fav_fruits.includes('bananas')) {
+    console.log("I really like bananas!");
+}
---
+++ app.ts
@@ -0,0 +1,23 @@
+let fav_fruits:string[] =["apple","orange","grapes"]
+if(fav_fruits.includes('apple')){
+    console.log("I really like apple!")
+}
+
+if(fav_fruits.includes('orange')){
+    console.log("I really like orange!")
+}
+
+
+if(fav_fruits.includes('mango')){
+    console.log("I really like mango!")
+}
+
+
+if(fav_fruits.includes('grapes')){
+    console.log("I really like grapes!")
+}
+
+
+if(fav_fruits.includes('bananas')){
+    console.log("I really like bananas!")
+}
